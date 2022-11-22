962358e0598286e38664bb7349d034d0f62d498d
Jakub Jánoška <jakub.janoska@gmail.com>
2022-11-22 20:22:44 +0100

    verzia 3, snaha vyuzitie zoznamu dvojic. susp. problem v nemoznosti rozpoznat 'nadpist'

--- rozriadkuj.py
+++ rozriadkuj.py
@@ -2,21 +2,22 @@ with open('git_codes.txt', encoding='utf-8') as soubor:
     text = soubor.read()
 
     ssplit = text.split()
-    zoznam = []
-    riadok = ""
-    for word in ssplit:
-        if 'git' not in word:
-            riadok = riadok + word + " "
-        elif 'git' in word:
-            zoznam.append(riadok)
-            riadok = word + " "
-            
+    zoznam = []  #bude zoznam dvojic (prikaz, popis)
+    prikaz = ""
+    popis = ''
+    #PrikazPrazdny, PopisPrazdny = True
+    for slovo in ssplit:
+        if '#' in slovo:
+            prikaz = text
+            text = slovo + ' '
+        #elif 'git' not in slovo:
+        #    prikaz = prikaz + slovo + " "
+        elif 'git' in slovo:
+            zoznam.append((prikaz, text))
+            prikaz = " "
+            text = slovo + ' '   
+        else:
+            text = text + slovo + " "
             
     print(*zoznam, sep='\n')
 
-    """    if 'git' in word:
-            print ('\n', 'git', end=' ')
-        else:
-            #word.rstrip()
-            print (word, end=' ')
-    """
