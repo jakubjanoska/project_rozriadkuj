dd8a57fa1d84995447f1bd8133012864828eb856
Jakub Jánoška <jakub.janoska@gmail.com>
2022-11-22 19:50:51 +0100

    prekopane, kazdy riadok je samostatny prvok zoznamu

--- rozriadkuj.py
+++ rozriadkuj.py
@@ -2,14 +2,21 @@ with open('git_codes.txt', encoding='utf-8') as soubor:
     text = soubor.read()
 
     ssplit = text.split()
+    zoznam = []
+    riadok = ""
     for word in ssplit:
-        if 'git' in word:
+        if 'git' not in word:
+            riadok = riadok + word + " "
+        elif 'git' in word:
+            zoznam.append(riadok)
+            riadok = word + " "
+            
+            
+    print(*zoznam, sep='\n')
+
+    """    if 'git' in word:
             print ('\n', 'git', end=' ')
         else:
             #word.rstrip()
             print (word, end=' ')
-
-    
-    
-
-    
+    """
