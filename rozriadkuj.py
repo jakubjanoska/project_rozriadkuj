a94b6655023322a315327816c6919a988f36123c
Jakub Jánoška <jakub.janoska@gmail.com>
2022-11-24 08:02:33 +0100

    Finalna verze projektu, updatovany gitignore

--- rozriadkuj.py
+++ rozriadkuj.py
@@ -1,23 +1,36 @@
-with open('git_codes.txt', encoding='utf-8') as soubor:
-    text = soubor.read()
+"""
+Cvičný program. 
+Zoberie vstupný text (text vykopírovaný z PDF https://programmingwithmosh.com/wp-content/uploads/2020/09/Git-Cheat-Sheet.pdf)
+a vytvorí z neho tabulku v dokumente  (v predpotopnom) .html
+Tabulka nie je dokonalá, bol som zvedavý, či to zvádnem roztriediť. 
+Následne test (a hladanie chýb) pri printe do .html
+"""
 
-    ssplit = text.split()
+with open('git_codes.txt', encoding='utf-8') as soubor:
+    vstup = soubor.read()
+
+    ssplit = vstup.split()
     zoznam = []  #bude zoznam dvojic (prikaz, popis)
-    prikaz = ""
+    prikaz = ''
     popis = ''
-    #PrikazPrazdny, PopisPrazdny = True
+    text = ''
     for slovo in ssplit:
         if '#' in slovo:
             prikaz = text
             text = slovo + ' '
-        #elif 'git' not in slovo:
-        #    prikaz = prikaz + slovo + " "
         elif 'git' in slovo:
+            prikaz = '<tr> <td>' +prikaz + '</td>'
+            text = '<td>' + text + '</td></tr>'
             zoznam.append((prikaz, text))
             prikaz = " "
             text = slovo + ' '   
         else:
             text = text + slovo + " "
-            
-    print(*zoznam, sep='\n')
 
+with open('tab3.html', mode='w', encoding='utf-8') as vysledok:
+    print('<!DOCTYPE html> <head> <title> Tabulka prikazu </title> </head> <body> Tu snad mozem nieco napisat', file=vysledok)
+    print('<table border="1">', file=vysledok)
+    for radek in zoznam:
+        for blok in radek:
+            print(blok, sep='', file=vysledok)
+    print('</table> </body> </html>', file=vysledok)
